ee8bc22acfba1ae02cc3b783626aa5cafa75680e
kuidi <hellosc@qq.com>
2022-08-18 15:46:01 +0800

    feat: 增加 province/index.js 文件

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const PATH_CHINA = path.join(__dirname, './$adcode_full.json')
 const PATH_PROVINCE_DIR = path.join(__dirname, './provinces')
 const DOWNLOAD_URL = 'https://geo.datav.aliyun.com/areas_v3/bound/$adcode_full.json'
 const PATH_CITY_FILE = PATH_PROVINCE_DIR + '/$adcode_full.json'
+const PATH_CITY_INDEX_FILE = PATH_PROVINCE_DIR + '/index.js'
 
 const downloadGeojson = async adcode => {
   const url = DOWNLOAD_URL.replace('$adcode', adcode)
@@ -14,7 +15,7 @@ const downloadGeojson = async adcode => {
 }
 
 const writeFile = ( filePath, geojson ) => {
-  writeFileSync(filePath, JSON.stringify(geojson))
+  writeFileSync(filePath, typeof geojson === 'string' ? geojson : JSON.stringify(geojson))
 }
 
 const download = async (adcode, filePath) => {
@@ -25,6 +26,15 @@ const download = async (adcode, filePath) => {
   return geojson
 }
 
+const generateProvinceIndexJs = async properties => {
+  let content = properties.map(property => `import ${property.name} from './${property.adcode}_full.json'`).join('\n')
+  content += '\n\nexport {\n'
+  content += properties.map(property => `  ${property.name},`).join('\n')
+  content += '\n}\n'
+  await writeFile(PATH_CITY_INDEX_FILE, content)
+  console.log('[generage success]: ', PATH_CITY_INDEX_FILE)
+}
+
 /**
  * 下载省份和省份包含的所有城市的 geojson 数据
  * @param {string} adcode 省份 / 城市 编码
@@ -33,8 +43,15 @@ const main = async adcode => {
   // 1. 下载省份数据
   const geojson = await download(adcode, PATH_CHINA)
   // 2. 遍历省份 geojson 数据下载城市数据
-  const cityDownloadProcess = geojson.features.map(feature => download(feature.properties.adcode, PATH_CITY_FILE))
+  const loadedFeatures = []
+  const cityDownloadProcess = geojson.features.map(feature => {
+    return download(feature.properties.adcode, PATH_CITY_FILE)
+      .then(() => loadedFeatures.push(feature))
+      .catch(err => console.log(err.message, feature.properties.adcode))
+  })
   await Promise.all(cityDownloadProcess)
+  // 3. 生成 province/index.js
+  await generateProvinceIndexJs(loadedFeatures.map(feature => feature.properties))
 }
 
 main('100000')
---
+++ provinces/index.js
@@ -0,0 +1,69 @@
+import 海南省 from './460000_full.json'
+import 天津市 from './120000_full.json'
+import 陕西省 from './610000_full.json'
+import 上海市 from './310000_full.json'
+import 山西省 from './140000_full.json'
+import 宁夏回族自治区 from './640000_full.json'
+import 河北省 from './130000_full.json'
+import 北京市 from './110000_full.json'
+import 江西省 from './360000_full.json'
+import 内蒙古自治区 from './150000_full.json'
+import 青海省 from './630000_full.json'
+import 云南省 from './530000_full.json'
+import 黑龙江省 from './230000_full.json'
+import 甘肃省 from './620000_full.json'
+import 浙江省 from './330000_full.json'
+import 澳门特别行政区 from './820000_full.json'
+import 广西壮族自治区 from './450000_full.json'
+import 湖南省 from './430000_full.json'
+import 福建省 from './350000_full.json'
+import 重庆市 from './500000_full.json'
+import 贵州省 from './520000_full.json'
+import 湖北省 from './420000_full.json'
+import 江苏省 from './320000_full.json'
+import 四川省 from './510000_full.json'
+import 安徽省 from './340000_full.json'
+import 广东省 from './440000_full.json'
+import 香港特别行政区 from './810000_full.json'
+import 西藏自治区 from './540000_full.json'
+import 河南省 from './410000_full.json'
+import 新疆维吾尔自治区 from './650000_full.json'
+import 辽宁省 from './210000_full.json'
+import 吉林省 from './220000_full.json'
+import 山东省 from './370000_full.json'
+
+export {
+  海南省,
+  天津市,
+  陕西省,
+  上海市,
+  山西省,
+  宁夏回族自治区,
+  河北省,
+  北京市,
+  江西省,
+  内蒙古自治区,
+  青海省,
+  云南省,
+  黑龙江省,
+  甘肃省,
+  浙江省,
+  澳门特别行政区,
+  广西壮族自治区,
+  湖南省,
+  福建省,
+  重庆市,
+  贵州省,
+  湖北省,
+  江苏省,
+  四川省,
+  安徽省,
+  广东省,
+  香港特别行政区,
+  西藏自治区,
+  河南省,
+  新疆维吾尔自治区,
+  辽宁省,
+  吉林省,
+  山东省,
+}
